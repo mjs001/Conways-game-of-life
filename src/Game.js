5518cb53b1a5fafde0262170278eb565cede7a0e
mjs001 <maljayd42@gmail.com>
2020-08-24 16:32:10 -0500

    made a grid

---
+++ src/Game.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Game.css';
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {
+    render(){
+        return(
+            <div className="Board" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
+                
+            </div>
+        )
+    }
+}
+
+export default Game;
